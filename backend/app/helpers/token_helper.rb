269ee9f353f9262d80b25e2e1910d83b0b0ada62
Jack Norquist <jack.norquist@outlook.com>
2024-05-28 15:42:05 -0500

    authentication

--- backend/app/helpers/token_helper.rb
+++ backend/app/helpers/token_helper.rb
@@ -3,12 +3,11 @@
 
 module TokenService
     def generate_token(user_id)
-      JWT.encode({ user_id: user_id }, Rails.application.secrets.secret_key_base)
+      JWT.encode({ user_id: user_id }, Rails.application.credentials.secret_key_base)
     end
 
     def decode_token(token)
-      puts "hello again"
-      JWT.decode(token, Rails.application.secrets.secret_key_base).first
+      JWT.decode(token, Rails.application.credentials.secret_key_base).first
     rescue JWT::DecodeError
       nil
     end

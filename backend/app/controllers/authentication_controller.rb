61e03bbb03d79f0be451e315e2d02a9077544c6f
Jack Norquist <jack.norquist@outlook.com>
2024-07-24 19:52:58 -0500

    syntax error in authentication controller

--- backend/app/controllers/authentication_controller.rb
+++ backend/app/controllers/authentication_controller.rb
@@ -8,9 +8,9 @@ class AuthenticationController < ApplicationController
       user = User.find_by(username: login_params[:username])
       if user && user.authenticate(login_params[:password])
         token = generate_token(user.id)
-        render json: {user: user_json(user), token: token}, status: :ok
+        render json: { user: user_json(user), token: token }, status: :ok
       else
-        render json: { error: 'Invalid useranem or password' }, status: :unauthorized
+        render json: { error: 'Invalid username or password' }, status: :unauthorized
       end
     end
 
